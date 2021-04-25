5ddeaed5c5bb3ba8ada2e7084a852339d193efde
Rey <rey.robles72@gmail.com>
2021-04-25 17:28:29 -0500

    Add files via upload

---
+++ detectMetaMask.js
@@ -0,0 +1,12 @@
+// This function detects most providers injected at window.ethereum
+import detectEthereumProvider from '@metamask/detect-provider';
+
+const provider = await detectEthereumProvider();
+
+if (provider) {
+  // From now on, this should always be true:
+  // provider === window.ethereum
+  startApp(provider); // initialize your app
+} else {
+  console.log('Please install MetaMask!');
+}
